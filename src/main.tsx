8c3b03fb58492232a887226215217e507810402f
neevan-gta99 <naveengta8581@gmail.com>
2025-10-28 14:01:39 +0530

    Strict mode commented from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,11 @@ import { store } from './redux/store/store';
 import React from 'react';
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
